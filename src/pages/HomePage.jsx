43417f0dd56aa46c76b6530fb3ef63e9b4244cc1
Mohamad Sahlan Umar <msahlanumar@gmail.com>
2025-08-07 19:37:26 +0700

    add home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center text-gray-800 my-4">
+        My To-Do List
+      </h1>
+      {/* Komponen-komponen lain akan ditambahkan di sini nanti */}
+    </div>
+  );
+}
+
+export default HomePage;
